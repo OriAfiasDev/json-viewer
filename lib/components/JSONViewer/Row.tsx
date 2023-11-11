db9ec11f435f350c4869fed71e5c75c198145252
Ori Afias <ori@afias.dev>
2023-11-11 22:43:22 +0200

    css

--- lib/components/JSONViewer/Row.tsx
+++ lib/components/JSONViewer/Row.tsx
@@ -19,6 +19,8 @@ const Row: React.FC<RowProps> = ({ row, theme, options, isCollapsed, isMiddleOfB
   const isCollapsable = !row.isPrimitive && row.position === 'start';
   const shouldDisplayLeftPanel = options.showLineNumbers || options.collapse.enabled;
 
+  const collapseIconProps = { height: 16, width: 16, className: getClassName(['collapseIcon']) };
+
   const onCollapseClick = useCallback(() => {
     onCollapse(row.lineNumber, isCollapsed ? 'expand' : 'collapse');
   }, [row.lineNumber, isCollapsed, onCollapse]);
@@ -26,30 +28,28 @@ const Row: React.FC<RowProps> = ({ row, theme, options, isCollapsed, isMiddleOfB
   if (isCollapsed && isMiddleOfBlock) return null;
 
   return (
-    <div className={getClassName(['wrapper'])}>
-      <span className={classes.container}>
-        {shouldDisplayLeftPanel && (
-          <span className={getClassName(['leftPanelContainer'])}>
-            <span className={getClassName(['text', 'lineNumber'])}>
-              {options.showLineNumbers && row.lineNumber}
-              <span
-                className={getClassName(['iconContainer', options.collapse.enabled && isCollapsable ? '' : 'hidden'])}
-                onClick={onCollapseClick}
-              >
-                {isCollapsed ? <Expand height={12} width={12} /> : <Collapse height={12} width={12} />}
-              </span>
+    <span className={classes.container}>
+      {shouldDisplayLeftPanel && (
+        <span className={getClassName(['leftPanelContainer'])}>
+          <span className={getClassName(['text', 'lineNumber'])}>
+            {options.showLineNumbers && String(row.lineNumber).padStart(2, '0')}
+            <span
+              className={getClassName(['iconContainer', options.collapse.enabled && isCollapsable ? '' : 'hidden'])}
+              onClick={onCollapseClick}
+            >
+              {isCollapsed ? <Expand {...collapseIconProps} /> : <Collapse {...collapseIconProps} />}
             </span>
           </span>
-        )}
-        <span className={getClassName(['indent', `indent-${options.tabSize}-${row.depth}`])} />
-        {options.beforeLine?.(row)}
-        {row.key && <span className={getClassName(['text', 'key'])}>"{row.key}":&nbsp;</span>}
-        <span className={getClassName(['text', 'bracket'])}>{displayBrackets(row)}</span>
-        {row.isPrimitive && <span className={getClassName(['text', row.valueType])}>{`${row.value}`}</span>}
-        {!row.isLast && row.position !== 'start' && <span className={getClassName(['text', 'bracket'])}>,</span>}
-        {options.afterLine?.(row)}
-      </span>
-    </div>
+        </span>
+      )}
+      <span className={getClassName(['indent', `indent-${options.tabSize}-${row.depth}`])} />
+      {options.beforeLine?.(row)}
+      {row.key && <span className={getClassName(['text', 'key'])}>"{row.key}":&nbsp;</span>}
+      <span className={getClassName(['text', 'bracket'])}>{displayBrackets(row)}</span>
+      {row.isPrimitive && <span className={getClassName(['text', row.valueType])}>{`${row.value}`}</span>}
+      {!row.isLast && row.position !== 'start' && <span className={getClassName(['text', 'bracket'])}>,</span>}
+      {options.afterLine?.(row)}
+    </span>
   );
 };
 
