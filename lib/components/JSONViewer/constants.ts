c8472b991f1a7c07f9bd02af8067fa005585f596
Ori Afias <ori@afias.dev>
2023-11-11 14:49:19 +0200

    remove comments

--- lib/components/JSONViewer/constants.ts
+++ lib/components/JSONViewer/constants.ts
@@ -8,22 +8,3 @@ export const DEFAULT_OPTIONS: Options = {
     enabled: true,
   },
 };
-
-// export const DEFAULT_THEME: Theme = {
-//   fontFamily: 'roboto mono',
-//   backgroundColor: '#fff',
-//   colors: {
-//     lineNumber: '#98A2B3',
-//     key: '#5048E5',
-//     bracket: '#344054',
-//     object: '#344054',
-//     array: '#344054',
-//     string: '#00ae6b',
-//     number: '#875afb',
-//     boolean: '#d72e82',
-//     null: '#f2283c',
-//   },
-//   hover: {
-//     row: '#F0F3FF',
-//   },
-// };
