71ac23275ad99188559a8a958192be1134731656
Ori Afias <ori@afias.dev>
2024-02-10 16:46:40 +0200

    stories

--- src/stories/JSONViewer.stories.ts
+++ src/stories/JSONViewer.stories.ts
@@ -13,12 +13,14 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const DefaultLight: Story = {
+  name: 'Theme/Light',
   args: {
     json: `{"foo": "bar"}`,
   },
 };
 
 export const DefaultDark: Story = {
+  name: 'Theme/Dark',
   args: {
     json: `{"foo": "bar"}`,
     theme: 'dark',
@@ -26,20 +28,46 @@ export const DefaultDark: Story = {
 };
 
 export const TabSize2: Story = {
+  name: 'tabSize/2',
   args: {
     json: `{"foo": "bar"}`,
     tabSize: 2,
   },
 };
 
+export const TabSize4: Story = {
+  name: 'tabSize/4',
+  args: {
+    json: `{"foo": "bar"}`,
+    tabSize: 4,
+  },
+};
+
+export const LineNumbers: Story = {
+  name: 'showLineNumbers/True',
+  args: {
+    json: `{"foo": "bar"}`,
+  },
+};
+
 export const NoLineNumbers: Story = {
+  name: 'showLineNumbers/False',
   args: {
     json: `{"foo": "bar"}`,
     showLineNumbers: false,
   },
 };
 
+export const Collapse: Story = {
+  name: 'collapseEnabled/True',
+  args: {
+    json: `{"foo": "bar"}`,
+    collapseEnabled: true,
+  },
+};
+
 export const NoCollapse: Story = {
+  name: 'collapseEnabled/False',
   args: {
     json: `{"foo": "bar"}`,
     collapseEnabled: false,
@@ -47,8 +75,18 @@ export const NoCollapse: Story = {
 };
 
 export const CollapseOnLoad: Story = {
+  name: 'collapsedOnLoad/True',
   args: {
     json: `{"foo": "bar"}`,
+    collapseEnabled: true,
     collapsedOnLoad: true,
+  },
+};
+
+export const NoCollapseOnLoad: Story = {
+  name: 'collapsedOnLoad/False',
+  args: {
+    json: `{"foo": "bar"}`,
+    collapsedOnLoad: false,
   },
 };
